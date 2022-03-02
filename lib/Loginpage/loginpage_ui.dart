ef0066a944dc833ab679d99b98e136c974217eee
nikhil1716 <nikhilgadhave875@gmail.com>
2022-03-02 12:01:01 +0530

    loginui

--- lib/Loginpage/loginpage_ui.dart
+++ lib/Loginpage/loginpage_ui.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
-
-import '../Signup_Page/signpage_ui.dart';
+import 'package:srs/homepage/homepage_ui.dart';
 
 class Loginhomepage extends StatefulWidget {
   const Loginhomepage({Key? key}) : super(key: key);
@@ -15,8 +14,8 @@ class Loginhomepage extends StatefulWidget {
 class _LoginhomepageState extends State<Loginhomepage> {
   var _passwordVisible;
   var loggedIn = false;
-  TextEditingController UseridCon = TextEditingController();
-  TextEditingController PassCon = TextEditingController();
+  TextEditingController useridController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   void initState() {
@@ -30,46 +29,63 @@ class _LoginhomepageState extends State<Loginhomepage> {
     return Scaffold(
         body: SingleChildScrollView(
       child: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30.h,
-            ),
-            Text(
-              "Login",
-              style: TextStyle(fontSize: 40),
-            ),
-            SizedBox(
-              height: 5.h,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: TextField(
-                controller: UseridCon,
-                keyboardType: TextInputType.visiblePassword,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30.h,
+              ),
+              Text(
+                "Login",
+                style: TextStyle(fontSize: 40),
+              ),
+              SizedBox(
+                height: 5.h,
+              ),
+              TextField(
+                controller: useridController,
+                style: TextStyle(color: Colors.black),
+                cursorColor: Colors.black,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none),
-                  filled: true,
-                  fillColor: Color(0xFFe7edeb),
-                  hintText: "User-ID",
+                  // errorText: _emailValidate ? 'Email field can\'t be empty :(' : null
+
+                  border: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.black, width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.black, width: 3.0),
+                  ),
+                  labelText: "User-ID",
+                  labelStyle: TextStyle(color: Colors.black),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: TextField(
-                controller: PassCon,
+              SizedBox(
+                height: 30,
+              ),
+              TextField(
+                controller: passwordController,
                 keyboardType: TextInputType.visiblePassword,
                 obscureText: !_passwordVisible,
+                style: TextStyle(color: Colors.black),
+                cursorColor: Colors.black,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide.none),
-                  filled: true,
-                  fillColor: Color(0xFFe7edeb),
-                  hintText: "Password",
+                  // errorText: _emailValidate ? 'Email field can\'t be empty :(' : null
+
+                  border: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.black, width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.black, width: 3.0),
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: Colors.black),
                   suffix: Padding(
                     padding: const EdgeInsets.only(right: 1.0),
                     child: GestureDetector(
@@ -89,64 +105,66 @@ class _LoginhomepageState extends State<Loginhomepage> {
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 5.h,
-            ),
-            GestureDetector(
-              onTap: () {}
-              // validateData()
-              ,
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 20.0.w),
-                height: 7.0.h,
-                width: 40.0.w,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 17, 17, 17),
-                        Color.fromARGB(255, 15, 15, 15)
-                      ],
-                      begin: FractionalOffset(0.0, 0.0),
-                      end: FractionalOffset(0.5, 0.0),
-                      stops: [0.0, 1.0],
-                      tileMode: TileMode.clamp),
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(30),
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30)),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ))
+                  ],
                 ),
-                child: Center(
-                    child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(255, 247, 246, 246),
-                    fontSize: 16.0.sp,
-                  ),
-                )),
               ),
-            ),
-            SizedBox(
-              height: 5.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account? "),
-                GestureDetector(
-                    onTap: () {
-                      Get.to(SignupPage());
-                    },
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15),
-                    ))
-              ],
-            )
-          ],
+              SizedBox(
+                height: 5.h,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.to(HomeUi());
+                }
+                // validateData()
+                ,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0.w),
+                  height: 7.0.h,
+                  width: 40.0.w,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 17, 17, 17),
+                          Color.fromARGB(255, 15, 15, 15)
+                        ],
+                        begin: FractionalOffset(0.0, 0.0),
+                        end: FractionalOffset(0.5, 0.0),
+                        stops: [0.0, 1.0],
+                        tileMode: TileMode.clamp),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30)),
+                  ),
+                  child: Center(
+                      child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 247, 246, 246),
+                      fontSize: 16.0.sp,
+                    ),
+                  )),
+                ),
+              ),
+              SizedBox(
+                height: 5.h,
+              ),
+            ],
+          ),
         ),
       ),
     ));
---
+++ lib/homepage/homepage_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeUi extends StatefulWidget {
+  const HomeUi({Key? key}) : super(key: key);
+
+  @override
+  _HomeUiState createState() => _HomeUiState();
+}
+
+class _HomeUiState extends State<HomeUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
